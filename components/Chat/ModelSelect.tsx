1489ff102610aa7c3c13fb3254ceda212c4ec119
dvk31 <125421041+dvk31@users.noreply.github.com>
2023-11-08 22:44:22 +0000

    added gpt4 120K model

--- components/Chat/ModelSelect.tsx
+++ components/Chat/ModelSelect.tsx
@@ -4,7 +4,7 @@ import HomeContext from '@/pages/api/home/home.context';
 
 export const ModelSelect = () => {
   const { t } = useTranslation('chat');
-
+  
   const {
     state: { selectedConversation },
     handleUpdateConversation,
@@ -14,29 +14,34 @@ export const ModelSelect = () => {
   const hardcodedModelId = 'gpt-4-1106-preview';
   const hardcodedModelName = 'GPT-4-1106 Preview';
 
-  // Since the model is hardcoded, the handleChange can simply pass the hardcoded model
   const handleChange = () => {
-    handleUpdateConversation(selectedConversation, {
-      key: 'model',
-      value: { id: hardcodedModelId, name: hardcodedModelName },
-    });
+    if (selectedConversation) {
+      handleUpdateConversation(selectedConversation, {
+        key: 'model',
+        value: { id: hardcodedModelId, name: hardcodedModelName },
+      });
+    }
+  };
+
+  // Safely get the translated string or return the key itself
+  const safeT = (key: string): string => {
+    const translation = t(key);
+    return typeof translation === 'string' ? translation : key;
   };
 
   return (
     <div className="flex flex-col">
       <label className="mb-2 text-left text-neutral-700 dark:text-neutral-400">
-        {t('Model')}
+        {safeT('Model')} {/* Use safeT instead of t */}
       </label>
       <div className="w-full rounded-lg border border-neutral-200 bg-transparent pr-2 text-neutral-900 dark:border-neutral-600 dark:text-white">
-        {/* The select input is disabled since the model is hardcoded */}
         <select
           className="w-full bg-transparent p-2"
-          placeholder={t('Select a model')}
+          aria-label={safeT('Select a model')} // Use safeT instead of t
           value={hardcodedModelId}
           onChange={handleChange}
           disabled={true}
         >
-          {/* Only one option is available which is the hardcoded model */}
           <option
             key={hardcodedModelId}
             value={hardcodedModelId}
