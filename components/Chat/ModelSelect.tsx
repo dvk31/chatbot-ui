7d9b1c1062c37989d404ea81437040d442913b82
dvk31 <125421041+dvk31@users.noreply.github.com>
2023-11-08 22:34:52 +0000

    added gpt4 120K model

--- components/Chat/ModelSelect.tsx
+++ components/Chat/ModelSelect.tsx
@@ -1,29 +1,25 @@
-import { IconExternalLink } from '@tabler/icons-react';
 import { useContext } from 'react';
-
 import { useTranslation } from 'next-i18next';
-
-import { OpenAIModel } from '@/types/openai';
-
 import HomeContext from '@/pages/api/home/home.context';
 
 export const ModelSelect = () => {
   const { t } = useTranslation('chat');
 
   const {
-    state: { selectedConversation, models, defaultModelId },
+    state: { selectedConversation },
     handleUpdateConversation,
-    dispatch: homeDispatch,
   } = useContext(HomeContext);
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    selectedConversation &&
-      handleUpdateConversation(selectedConversation, {
-        key: 'model',
-        value: models.find(
-          (model) => model.id === e.target.value,
-        ) as OpenAIModel,
-      });
+  // Hardcoded model ID and name
+  const hardcodedModelId = 'gpt-4-1106-preview';
+  const hardcodedModelName = 'GPT-4-1106 Preview';
+
+  // Since the model is hardcoded, the handleChange can simply pass the hardcoded model
+  const handleChange = () => {
+    handleUpdateConversation(selectedConversation, {
+      key: 'model',
+      value: { id: hardcodedModelId, name: hardcodedModelName },
+    });
   };
 
   return (
@@ -32,35 +28,25 @@ export const ModelSelect = () => {
         {t('Model')}
       </label>
       <div className="w-full rounded-lg border border-neutral-200 bg-transparent pr-2 text-neutral-900 dark:border-neutral-600 dark:text-white">
+        {/* The select input is disabled since the model is hardcoded */}
         <select
           className="w-full bg-transparent p-2"
-          placeholder={t('Select a model') || ''}
-          value={selectedConversation?.model?.id || defaultModelId}
+          placeholder={t('Select a model')}
+          value={hardcodedModelId}
           onChange={handleChange}
+          disabled={true}
         >
-          {models.map((model) => (
-            <option
-              key={model.id}
-              value={model.id}
-              className="dark:bg-[#343541] dark:text-white"
-            >
-              {model.id === defaultModelId
-                ? `Default (${model.name})`
-                : model.name}
-            </option>
-          ))}
+          {/* Only one option is available which is the hardcoded model */}
+          <option
+            key={hardcodedModelId}
+            value={hardcodedModelId}
+            className="dark:bg-[#343541] dark:text-white"
+          >
+            {hardcodedModelName}
+          </option>
         </select>
       </div>
-      <div className="w-full mt-3 text-left text-neutral-700 dark:text-neutral-400 flex items-center">
-        <a
-          href="https://platform.openai.com/account/usage"
-          target="_blank"
-          className="flex items-center"
-        >
-          <IconExternalLink size={18} className={'inline mr-1'} />
-          {t('View Account Usage')}
-        </a>
-      </div>
+      {/* ...rest of your component */}
     </div>
   );
 };
